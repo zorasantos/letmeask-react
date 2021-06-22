673a0c5d923985bd08be98b9efb3c2546b38b24c
Zora Santos <zoranildo22@gmail.com>
2021-06-22 08:56:24 -0300

    Estilização da page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,18 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 export function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas." />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <button>
             <img src={googleIconImg} alt="Logo do Google" />
@@ -21,7 +23,11 @@ export function Home() {
           <form>
             <input
               type="text"
+              placeholder="Digite o código da sala"
             />
+            <button type="submit">
+              Entrar na sala
+            </button>
           </form>
         </div>
       </main>
